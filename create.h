67eb3f57ff5d9db877039748e5ef73a28cabd5c7
ulturgashevme <162455564+ulturgashevme@users.noreply.github.com>
2024-05-18 19:59:41 +0300

    Update create.h

--- create.h
+++ create.h
@@ -1,1 +1,24 @@
+CacheNode* createNode(int key, int value) 
+{
+    CacheNode* node = (CacheNode*)malloc(sizeof(CacheNode));
 
+    node->key = key;
+    node->value = value;
+    node->freq = 1;
+    node->prev = NULL;
+    node->next = NULL;
+
+    return node;
+}
+
+Cache* createCache(int capacity) 
+{
+    Cache* cache = (Cache*)malloc(sizeof(Cache));
+
+    cache->capacity = capacity;
+    cache->size = 0;
+    cache->head = NULL;
+    cache->tail = NULL;
+
+    return cache;
+}
