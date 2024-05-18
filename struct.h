5b4f5bea07fa9f8b569298355bc5e047a988d433
ulturgashevme <162455564+ulturgashevme@users.noreply.github.com>
2024-05-18 19:02:51 +0300

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,27 +1,15 @@
-typedef struct LFUNode {
+typedef struct CacheNode {
     int key;              
     int value;             
     int frequency;          
-    struct LFUNode* next;  
-    struct LFUNode* prev;  
-} LFUNode;
+    struct CacheNode* next;  
+    struct CacheNode* prev;  
+} CacheNode;
 
-typedef struct LFUCache {
+typedef struct Cache {
     int capacity;                   
     int size;                       
     LFUNode *head;
     LFUNode *tail;
-} LFUCache;
+} Cache;
 
-LFUNode* LFUcreateNode(int key, int value) 
-{
-    LFUNode* node = (LFUNode*)malloc(sizeof(LFUNode));
-
-    node->key = key;
-    node->value = value;
-    node->frequency = 1;
-    node->prev = NULL;
-    node->next = NULL;
-
-    return node;
-}
