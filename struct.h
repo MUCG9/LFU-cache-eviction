65bb1f18fcd1ece0ad5c6e03df00e0314f21a98d
ulturgashevme <162455564+ulturgashevme@users.noreply.github.com>
2024-05-18 18:11:58 +0300

    Basic LFU Cache Data Structures

--- struct.h
+++ struct.h
@@ -1,1 +1,14 @@
+typedef struct LFUNode {
+    int key;              
+    int value;             
+    int frequency;          
+    struct LFUNode* next;  
+    struct LFUNode* prev;  
+} LFUNode;
 
+typedef struct LFUCache {
+    int capacity;                   
+    int size;                       
+    LFUNode *head;
+    LFUNode *tail;
+} LFUCache;
