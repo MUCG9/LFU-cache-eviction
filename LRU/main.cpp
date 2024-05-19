8f00568aec3535496f41076cd6b12d712092da73
ulturgashevme <162455564+ulturgashevme@users.noreply.github.com>
2024-05-19 18:44:27 +0300

    Update main.cpp

--- LRU/main.cpp
+++ LRU/main.cpp
@@ -223,7 +223,7 @@ int main()
     printf("%d\n", hits);
 
     elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
-    printf("LFU time: %f sec\n", elapsed);
+    printf("LRU time: %f sec\n", elapsed);
 
     return 0;
 }
