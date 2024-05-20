0fb026d4589b2ec8edad514bd8ed47f1009c7e3a
ArFar1975 <142601704+ArFar1975@users.noreply.github.com>
2024-05-20 16:32:42 +0300

    Update compile_main.bat

--- LFU/compile_main.bat
+++ LFU/compile_main.bat
@@ -1,4 +1,4 @@
-set FLAGS =-Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wno-missing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE
+set FLAGS =-Wall -Wextra -pedantic -std=c++11 -g --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose
 g++ create.cpp -c %FLAGS%
 g++ lfu_cache.cpp -c %FLAGS%
 g++ main.cpp -c %FLAGS%
